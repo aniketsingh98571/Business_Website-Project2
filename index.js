fd4a4de1d4e1918cd99718cc865c2aa5c5b19ecb
Aniket Singh <thor98571@gmail.com>
2021-02-17 12:00:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,3 +20,11 @@ function submit(e){
  })
     alert("Your email saved to our database")
 }
+(function(d, m){
+    var kommunicateSettings = 
+        {"appId":"43e42c6c49cf71079118a91dcca929fa","popupWidget":true,"automaticChatOpenOnNavigation":true};
+    var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+    s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+    var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+    window.kommunicate = m; m._globals = kommunicateSettings;
+})(document, window.kommunicate || {});
